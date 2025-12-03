21944155dc91a7251e4e1ac823caa6d95260e968
MinhKhanhFPTU188 <khanhpmhe176285@fpt.edu.vn>
2025-12-03 21:45:21 +0700

    feat: add template for coding practice

--- elios_FE/src/codingChallenge/components/ProblemDescription.js
+++ elios_FE/src/codingChallenge/components/ProblemDescription.js
@@ -5,7 +5,7 @@ import SubmissionsView from "./SubmissionsView";
 import SolutionView from "./SolutionView";
 import "../style/ProblemDescription.css"; 
 
-const ProblemDescription = ({ problemId }) => {
+const ProblemDescription = ({ problemId, problemData, loading, error }) => {
   const [activeTab, setActiveTab] = useState("description");
 
   return (
@@ -34,7 +34,13 @@ const ProblemDescription = ({ problemId }) => {
 
       {/* Tab Content */}
       <div id="tab-content">
-        {activeTab === "description" && <DescriptionView problemId={problemId} />}
+        {activeTab === "description" && (
+            <DescriptionView 
+                problem={problemData} 
+                loading={loading} 
+                error={error} 
+            />
+        )}
         {activeTab === "submissions" && <SubmissionsView problemId={problemId} />}
         {activeTab === "solution" && <SolutionView problemId={problemId} />}
       </div>
